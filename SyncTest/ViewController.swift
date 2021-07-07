2186d7193f61e68b7a4dcb99ea35bdf1198c8ff0
Matheus Reis <matheus.jesus@mobile2you.com.br>
2021-07-07 18:17:33 -0300

    Initial Commit

---
+++ SyncTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SyncTest
+//
+//  Created by mobile2you on 07/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
